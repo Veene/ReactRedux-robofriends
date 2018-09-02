944e2781bc10bc5d44b66713d8fb67a45fc50a4b
John Pawlak <johnpawlak@gmail.com>
2018-09-02 17:29:46 -0400

    full rework with redux complete

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'; //from react redux pacakage
-import { createStore } from 'redux'; //from redux package
+import { createStore, applyMiddleware, combineReducers } from 'redux'; // functions from redux package
+import { createLogger } from 'redux-logger'; // middleware function imported from redux logger
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-import { searchRobots } from './reducers';
+import { searchRobots, requestRobots } from './reducers';
 import 'tachyons';
 // import Card from './Card.js';
 import App from './App';
 // import { robots } from './robots';
 
-const store = createStore(searchRobots)
+const logger = createLogger(); //middleware
+
+const rootReducer = combineReducers({ searchRobots, requestRobots })
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
 
 ReactDOM.render(   
                 <Provider store={store}>
